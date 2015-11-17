2d640b7c2b21c0f68a2bdc3cd931af7699d1c0d5
paupalou <paupalou@gmail.com>
2015-11-17 01:15:42 +0100

    fixes

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,8 +17,3 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-
-alias gls="gls -F --color"
-alias gl="gls -lAh --color"
-alias gll="gls -l --color"
-alias gla='gls -A --color'
