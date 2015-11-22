142f3ae73a6b005416c6c5310602afe4e533b6ac
paupalou <paupalou@gmail.com>
2015-11-22 17:45:53 +0100

    tmux better startup

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -1,7 +1,11 @@
 #/bin/bash
-if [[ -z "$TMUX" ]]; then 
-    ID="`tmux ls&>/dev/null | grep -vm1 attached | cut -d: -f1`" # get the id of a deattached session
-    if [[ ! -z "$ID" ]] ;then # if not available create a new one
-        tmux attach-session -t "$ID" # if available attach to it
+source $ZSH/script/colors
+if [[ -z "$TMUX" ]] ;then
+    ID="`tmux ls&>/dev/null | grep -vm1 attached | cut -d: -f1`"
+    if [[ ! -z "$ID" ]] ;then # if there is some deattached session print they
+        echo ''
+        printf "\r   $(pc "TMUX DEATTACHED SESSIONS" $lblue$uline) \n"
+        printf "\r   $(pc "$(tmux list-sessions)" $green)\n"
+        echo ''
     fi
 fi
