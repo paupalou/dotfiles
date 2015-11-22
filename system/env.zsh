5be440af4c8531dc4d2127f2f8382707170bc2aa
paupalou <paupalou@gmail.com>
2015-11-22 20:03:14 +0100

    idem

--- system/env.zsh
+++ system/env.zsh
@@ -2,5 +2,3 @@ export EDITOR='vim'
 
 # Setting ag as the default source for fzf
 export FZF_DEFAULT_COMMAND='ag -g ""'
-# To apply the command to CTRL-T as well
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
