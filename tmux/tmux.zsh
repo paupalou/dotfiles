b012630850490623eded52fe140be1be88755d62
paupalou <paupalou@gmail.com>
2015-11-22 17:27:39 +0100

    tmux simplified startup

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -1,9 +1,7 @@
-#!/bin/bash
-if [[ -z "$TMUX" ]] ;then
-    ID="`tmux ls | grep -vm1 attached | cut -d: -f1`" # get the id of a deattached session
-    if [[ -z "$ID" ]] ;then # if not available create a new one
-        tmux new-session
-    else
+#/bin/bash
+if [[ -z "$TMUX" ]]; then 
+    ID="`tmux ls&>/dev/null | grep -vm1 attached | cut -d: -f1`" # get the id of a deattached session
+    if [[ ! -z "$ID" ]] ;then # if not available create a new one
         tmux attach-session -t "$ID" # if available attach to it
     fi
 fi
