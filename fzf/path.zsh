d608027d6ae0fd28e2c9c71732ec3e0edac02a61
paupalou <paupalou@gmail.com>
2016-09-29 17:39:38 +0200

    changed ripgrep for ag added nerdtree and vim-greeper added bin/rg (ripgrep)

--- fzf/path.zsh
+++ fzf/path.zsh
@@ -1,3 +1,5 @@
 # Ag as default source for fzf
-export FZF_DEFAULT_COMMAND='ag -g ""'
+# export FZF_DEFAULT_COMMAND='ag -g ""'
+# ripgreg as default source for fzf
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
