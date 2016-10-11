f45245ca7717924be6a4feaa9118ecae46da9c98
paupalou <paupalou@gmail.com>
2016-10-11 16:02:31 +0200

    better structure of topics, and remove things i dont use

--- fzf/path.zsh
+++ fzf/path.zsh
@@ -1,5 +1,3 @@
-# Ag as default source for fzf
-# export FZF_DEFAULT_COMMAND='ag -g ""'
 # ripgreg as default source for fzf
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='rg -i --files --no-ignore --follow --glob "!.git/*"'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
