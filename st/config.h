585cca989369c1aa10f7cd33e9f8eb14a283da16
paupalou <paupalou@gmail.com>
2016-11-25 15:13:32 +0100

    using minimal prompt for zprezto, changed a bit st config

--- st/config.h
+++ st/config.h
@@ -3,9 +3,9 @@
 /*
  * appearance
  *
- * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
+ * font: see
  */
-static char font[] = "Operator Mono:pixelsize=18:antialias=true:autohint=true";
+static char font[] = "Operator Mono:size=13:antialias=true:autohint=true";
 static int borderpx = 3;
 
 /*
@@ -16,7 +16,7 @@ static int borderpx = 3;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char shell[] = "/bin/sh";
+static char shell[] = "/usr/local/bin/zsh";
 static char *utmp = NULL;
 static char stty_args[] = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
