8e560a642d0666e93c48d374d3a108ae1cb8eac2
paupalou <paupalou@gmail.com>
2017-03-02 14:35:46 +0100

    fzf rg now ignore node_modules vim plugin to load local vimrc

--- fzf/path.zsh
+++ fzf/path.zsh
@@ -1,3 +1,3 @@
 # ripgreg as default source for fzf
-export FZF_DEFAULT_COMMAND='rg -i --files --no-ignore --follow --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='rg -i --files --no-ignore --follow --glob "!.git/*" --glob "!node_modules/*" '
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
