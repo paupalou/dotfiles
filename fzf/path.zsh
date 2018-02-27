73319506f1dded7450a16401a8b2892427d46805
Pau <paupalou@gmail.com>
2018-02-27 22:39:54 +0100

    Ignore python cache/pyo files.

--- fzf/path.zsh
+++ fzf/path.zsh
@@ -1,3 +1,7 @@
 # ripgreg as default source for fzf
-export FZF_DEFAULT_COMMAND='rg -i --files --no-ignore --follow --glob "!.git/*" --glob "!node_modules/*" '
+export FZF_DEFAULT_COMMAND='rg -i --files --no-ignore --follow \
+  --glob "!.git/*" \
+  --glob "!node_modules/*" \
+  --glob "!*.pyc" \
+  --glob "!venv/*" '
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
