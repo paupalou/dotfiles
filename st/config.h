d12b8828a83670115d1b103eb719f1121c0ccf5b
Pau <paupalou@gmail.com>
2018-11-04 00:07:34 +0100

    reduce 1px main font.

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "OperatorMonoLig Nerd Font:style=Book:weight=bold:pixelsize=17:antialias=true:autohint=true";
+static char *font = "OperatorMonoLig Nerd Font:style=Book:weight=bold:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 3;
 
 /*
