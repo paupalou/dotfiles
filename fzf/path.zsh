a1b41d169cae1155d907ccb489f8626d39dcd6ff
paupalou <paupalou@gmail.com>
2020-08-14 08:47:18 +0200

    FZF changed default command to use FD

--- fzf/path.zsh
+++ fzf/path.zsh
@@ -1,7 +1,3 @@
-# ripgreg as default source for fzf
-export FZF_DEFAULT_COMMAND='rg -i --files --no-ignore --follow \
-  --glob "!.git/*" \
-  --glob "!node_modules/*" \
-  --glob "!*.pyc" \
-  --glob "!venv/*" '
+# Setting fd as the default source for fzf
+export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
