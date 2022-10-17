99793c8ebfd0324cd10a167092668d5b9ba73209
Pau Palou <pau.palou@nccgroup.com>
2022-10-17 16:16:13 +0200

    fix lua config listing

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -8,5 +8,5 @@ require('plugins')
 
 -- require each plugin config file
 local conf_dir=os.getenv('XDG_CONFIG_HOME') or '~/.config'
-local list_files_command="ls -pa -I init.lua " ..conf_dir.. "/nvim/lua/plugins | sed -n 's/.lua$//p'"
+local list_files_command="ls -pa " ..conf_dir.. "/nvim/lua/plugins | grep -v init.lua | sed -n 's/.lua$//p'"
 for dir in io.popen(list_files_command):lines() do require('plugins/'..dir) end
