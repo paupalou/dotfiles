3013e3c2514936a3450da9629f0957d911d9affd
paupalou <paupalou@gmail.com>
2022-11-07 22:34:12 +0100

    hi neotree autoclose last window

--- neovim/.config/nvim/lua/plugins/neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,7 +1,9 @@
 local MAPPER = require('utils')
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-require("neo-tree").setup()
+require("neo-tree").setup({
+  close_if_last_window = true
+})
 
 MAPPER.map(MAPPER.MODES.NORMAL, '<C-n>', '<cmd>:Neotree toggle<CR>')
 MAPPER.map(MAPPER.MODES.NORMAL, '<leader>n', '<cmd>:Neotree reveal<CR>')
